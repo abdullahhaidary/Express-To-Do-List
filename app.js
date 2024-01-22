055f0b05067b8314d40326cc44f1b1c61e31962c
unknown <abdullahhadiary4262@gmail.com>
2024-01-22 12:16:05 +0430

    Remove Function Added

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express= require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended:false }));
-let item=[];
-var a = 3+4;
-var today=new Date();
-app.get("/",function(req, res){
-    res.render("index",{newItem:item});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let item = [];
+
+app.get("/", function (req, res) {
+    res.render("index", { newItem: item });
 });
+
 app.use(express.static("public"));
-app.post("/",function(req, res){
-    // console.log(req.body);
+
+app.post("/", function (req, res) {
     item.push(req.body.newItem);
     res.redirect("/");
 });
-app.listen(3000,function(){
+app.use(bodyParser.json());
+app.post('/removeItem', (req, res) => {
+    const { index } = req.body;
+    console.log(index);
+
+    // Remove the item from the array
+    item.splice(index, 1);
+
+    // Send a response indicating success
+    res.json({ success: true });
+});
+
+app.listen(3000, function () {
     console.log("The server has been run on localhost:3000");
-})
+});
